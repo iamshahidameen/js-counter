f4aa2a5e32ab318786fedef2a7b790f59d09f37a
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-21 01:40:04 +0500

    Made counter by setting innerText of div to 0 then used setTimout 1 and increment those numbers

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const counterEl = document.querySelectorAll('.counter');
+/*const counterEl = document.querySelectorAll('.counter');
 
 counterEl.forEach((e) => {
-    console.log(e.getAttribute('data-target'));
+    let attVal = e.getAttribute('data-target');
+    counterFunc();
+    function counterFunc(){
+        for(let i = 0; i <= attVal; i++ ){
+            document.write(i);
+        }
+    }
 })
+*/
+
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 200
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+
+    updateCounter()
+})
+
+
+
